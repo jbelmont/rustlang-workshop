2eeb4dfd77997f362b21caa0ccc90512b6485ca7
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2018-08-26 17:35:15 -0400

    Add if/let, placeholder and None/Some to enum examples.

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -3,6 +3,7 @@ enum Student {
     Sophomore,
     Junior,
     Senior,
+    None
 }
 
 impl Student {
@@ -12,11 +13,48 @@ impl Student {
             Student::Sophomore => String::from("Sophomore"),
             Student::Junior => String::from("Junior"),
             Student::Senior => String::from("Senior"),
+            _ => String::from("no student type found")
         }
+    }
+}
+
+fn multiply_by_five(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i * 5)
+    }
+}
+
+fn if_let_example(student: &Student) -> String {
+    if let Student::Senior = student {
+        "This is a Senior Student".to_string()
+    } else {
+        "Did not match here".to_string()
     }
 }
 
 fn main() {
     let student = Student::Freshman;
     println!("{}", student.find_student());
+
+    let no_student = Student::None;
+    println!("{}", no_student.find_student());
+
+    let student = Student::Sophomore;
+    println!("{}", student.find_student());
+
+    let student = Student::Junior;
+    println!("{}", student.find_student());
+
+    let student = Student::Senior;
+    println!("{}", student.find_student());
+
+    let nine = Some(9);
+    let fourty_five = multiply_by_five(nine);
+    println!("the some case is {:?}", fourty_five);
+
+    let none = multiply_by_five(None);
+    println!("the none case is {:?}", none);
+
+    println!("{}", if_let_example(&Student::Senior));
 }
