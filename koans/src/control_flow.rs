9e7861526b766a8f1abffa37799fe69fff7077d9
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2019-11-09 18:09:10 -0500

    Adding rest of document for control flow koan.

--- koans/src/control_flow.rs
+++ koans/src/control_flow.rs
@@ -33,4 +33,78 @@ pub fn control_flow() {
         _ => println!("Not a prime"),
     }
     assert!(word == "is a prime");
+
+    // rustlang has `if`, `else if`, `else` branch keywords like other
+    // languages
+    let x = 7;
+    let word;
+    if x < -1 {
+        word = "one".to_string();
+    } else if x == 5 {
+        word = "five".to_string();
+    } else {
+        word = "no matches".to_string();
+    }
+    assert!(word == "not sure");
+
+    // rust has a `loop` keyword to indicate an infinite loop
+    // similar to what you would do with a while(true) {} loop
+    // in c-type languages
+    
+    let mut counter = 1;
+    let mut sentence;
+
+    // infinite loop
+    // there is a keyword to break out of infinite loops
+    loop {
+        counter += 2;
+        if counter == 3 {
+            sentence = String::from("I am 3");
+            __;
+        } else if counter % 2 == 0 {
+            sentence = String::from("divisible by even number");
+        } else {
+            sentence = String::from("not sure here");
+        }
+    }
+    assert_eq!(sentence, __);
+
+    let mut count = 1;
+    let passphrase;
+    while count < 101 {
+        if count % 15 == 0 {
+            passphrase = "fizzbuzz";
+        } else if count % 3 == 0 {
+            passphrase = "fizz";
+        } else if count % 5 == 0 {
+            passphrase = "buzz";
+        } else {
+            passphrase = "";
+        }
+        count += 1;
+    }
+    assert_eq!(passphrase, __);
+
+    // rust also has a for loop except it is not like traditional for loop
+    // for (int i = 0; i < SOME_VAR; i++) {} 
+    // the above is not a valid for loop in rust
+    let n;
+    for num in 2..200 {
+        if num % 2 == 0 {
+            print!("what am i");
+            n = 0;
+        } else if num % 3 == 0 {
+            print!("do you know?");
+            n = 1;
+        } else {
+            print!("{}", num);
+            n = -1;
+        }
+    }
+    assert!(n == __);
+}
+
+#[test]
+fn test_controlflow() {
+    control_flow();
 }
